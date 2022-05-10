e82c15072441484040751bceac0a9ac285a4a9d6
lx6927 <951054143@qq.com>
2022-05-10 11:39:16 +0800

    db.js

---
+++ db.js
@@ -0,0 +1,15 @@
+// 引入easier_query
+const EasyQuery = require('easier_query')
+// 初始化easier_query
+const myQuery = new EasyQuery({
+    // host: "127.0.0.1",      // the mysql host
+    // host: "localhost",      // the mysql host
+    host: "",      // the mysql host
+    port: "3306",           // the mysql server port
+    user: "root",           // the mysql user
+    password: "",       // the password
+    database: "app",       // the database you will use
+    debug:true              // true means the built sql string will be printed in the console
+})
+// 导出实例化之后的easier_query
+module.exports = myQuery
